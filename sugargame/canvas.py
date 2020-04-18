15b9fa5ac3c9cb1cc73cf357cb5f338687bda8cf
JuiP <juipradhan2k@gmail.com>
2020-04-18 20:19:14 +0530

    Update to sugargame version=1.3

--- sugargame/canvas.py
+++ sugargame/canvas.py
@@ -1,63 +1,74 @@
+#
+# Copyright (c) 2020 Wade Brainerd
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+#
+
 import os
 import gi
 gi.require_version('Gtk', '3.0')
 from gi.repository import Gtk
-from gi.repository import GObject
 from gi.repository import GLib
+from sugar3.activity.activity import PREVIEW_SIZE
 import pygame
-import event
+import sugargame.event as event
 
 CANVAS = None
 
 
 class PygameCanvas(Gtk.EventBox):
 
-    """
-    mainwindow is the activity intself.
-    """
-
-    def __init__(self, mainwindow, pointer_hint=True):
-        GObject.GObject.__init__(self)
+    def __init__(self, activity, main=None, modules=[pygame]):
+        Gtk.EventBox.__init__(self)
 
         global CANVAS
         assert CANVAS is None, "Only one PygameCanvas can be created, ever."
         CANVAS = self
 
         # Initialize Events translator before widget gets "realized".
-        self.translator = event.Translator(mainwindow, self)
+        self.translator = event.Translator(activity, self)
 
-        self._mainwindow = mainwindow
+        self._activity = activity
+        self._main = main
+        self._modules = modules
 
         self.set_can_focus(True)
 
         self._socket = Gtk.Socket()
+        self._socket.connect('realize', self._realize_cb)
         self.add(self._socket)
 
         self._initialized = False
 
         self.show_all()
 
-    def run_pygame(self, main_fn):
-        # Run the main loop after a short delay.  The reason for
-        # the delay is that the Sugar activity is not properly
-        # created until after its constructor returns. If the Pygame
-        # main loop is called from the activity constructor, the
-        # constructor never returns and the activity freezes.
-        GLib.idle_add(self._run_pygame_cb, main_fn)
+    def _realize_cb(self, widget):
 
-    def _run_pygame_cb(self, main_fn):
-        if self._initialized:
-            return
         # Preinitialize Pygame with the X window ID.
-        os.environ['SDL_WINDOWID'] = str(self._socket.get_id())
-        if pygame.display.get_surface() is not None:
-            pygame.display.quit()
-        pygame.init()
+        os.environ['SDL_WINDOWID'] = str(widget.get_id())
+        for module in self._modules:
+        	module.init()
 
         # Restore the default cursor.
-        self._socket.props.window.set_cursor(None)
+        widget.props.window.set_cursor(None)
 
-        # Initialize the Pygame window.
+        # Confine the Pygame surface to the canvas size
         r = self.get_allocation()
         self._sceen = pygame.display.set_mode(
             (r.width, r.height), pygame.RESIZABLE)
@@ -65,10 +76,36 @@ class PygameCanvas(Gtk.EventBox):
         # Hook certain Pygame functions with GTK equivalents.
         self.translator.hook_pygame()
 
-        # Run the Pygame main loop.
-        main_fn()
-        self._initialized = True
-        return False
+        # Call the caller's main loop as an idle source
+        if self._main:
+            GLib.idle_add(self._main)
 
     def get_pygame_widget(self):
         return self._socket
+    
+    def get_preview(self):
+        """
+        Return preview of main surface
+        How to use in activity:
+            def get_preview(self):
+                return self.game_canvas.get_preview()
+        """
+
+        if not hasattr(self, '_screen'):
+            return None
+
+        _tmp_dir = os.path.join(self._activity.get_activity_root(),
+            'tmp')
+        _file_path = os.path.join(_tmp_dir, 'preview.png')
+
+        width = PREVIEW_SIZE[0]
+        height = PREVIEW_SIZE[1]
+        _surface = pygame.transform.scale(self._screen, (width, height))
+        pygame.image.save(_surface, _file_path)
+
+        f = open(_file_path, 'rb')
+        preview = f.read()
+        f.close()
+        os.remove(_file_path)
+
+        return preview
--- sugargame/event.py
+++ sugargame/event.py
@@ -1,5 +1,28 @@
+#
+# Copyright (c) 2020 Wade Brainerd
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+#
+
 import gi
 gi.require_version('Gtk', '3.0')
+import logging
 from gi.repository import GLib
 from gi.repository import Gdk
 import pygame
@@ -29,6 +52,8 @@ class Translator(object):
         'KP_Down': pygame.K_KP2,
         'KP_Left': pygame.K_KP4,
         'KP_Right': pygame.K_KP6,
+        'KP_Next' : pygame.K_KP3,
+        'KP_Begin' : pygame.K_KP5,
 
     }
 
@@ -41,16 +66,17 @@ class Translator(object):
         pygame.K_RSHIFT: pygame.KMOD_RSHIFT,
     }
 
-    def __init__(self, mainwindow, inner_evb):
+    def __init__(self, activity, inner_evb):
         """Initialise the Translator with the windows to which to listen"""
-        self._mainwindow = mainwindow
+        self._activity = activity
         self._inner_evb = inner_evb
 
         # Enable events
         # (add instead of set here because the main window is already realized)
-        self._mainwindow.add_events(
+        self._activity.add_events(
             Gdk.EventMask.KEY_PRESS_MASK |
-            Gdk.EventMask.KEY_RELEASE_MASK
+            Gdk.EventMask.KEY_RELEASE_MASK |
+            Gdk.EventMask.VISIBILITY_NOTIFY_MASK
         )
 
         self._inner_evb.set_events(
@@ -61,24 +87,22 @@ class Translator(object):
             Gdk.EventMask.BUTTON_RELEASE_MASK
         )
 
-        self._mainwindow.set_can_focus(True)
+        self._activity.set_can_focus(True)
         self._inner_evb.set_can_focus(True)
 
         # Callback functions to link the event systems
-        self._mainwindow.connect('unrealize', self._quit_cb)
-        self._mainwindow.connect(
+        self._activity.connect('unrealize', self._quit_cb)
+        self._activity.connect(
             'visibility_notify_event', self._visibility_cb)
+        self._activity.connect('configure-event', self._resize_cb)
         self._inner_evb.connect('key_press_event', self._keydown_cb)
         self._inner_evb.connect('key_release_event', self._keyup_cb)
         self._inner_evb.connect('button_press_event', self._mousedown_cb)
         self._inner_evb.connect('button_release_event', self._mouseup_cb)
         self._inner_evb.connect('motion-notify-event', self._mousemove_cb)
-        self._inner_evb.connect('draw', self._draw_cb)
-        self._inner_evb.connect('configure-event', self._resize_cb)
         self._inner_evb.connect('screen-changed', self._screen_changed_cb)
 
         # Internal data
-        self.__stopped = False
         self.__keystate = [0] * 323
         self.__button_state = [0, 0, 0]
         self.__mouse_pos = (0, 0)
@@ -95,31 +119,25 @@ class Translator(object):
         pygame.mouse.get_pos = self._get_mouse_pos
 
     def update_display(self):
-        pygame.event.post(pygame.event.Event(pygame.VIDEOEXPOSE))
+        if pygame.display.get_init():
+            pygame.event.post(pygame.event.Event(pygame.VIDEOEXPOSE))
 
     def _visibility_cb(self, widget, event):
-        if pygame.display.get_init():
-            self.update_display()
+        self.update_display()
         return False
 
     def _screen_changed_cb(self, widget, previous_screen):
-        if pygame.display.get_init():
-            self.update_display()
-
-    def _draw_cb(self, event, widget):
-        if pygame.display.get_init():
-            pygame.event.post(pygame.event.Event(pygame.VIDEOEXPOSE))
-        return True
+        self.update_display()
 
     def _resize_cb(self, widget, event):
-        evt = pygame.event.Event(pygame.VIDEORESIZE,
+        if pygame.display.get_init():
+            evt = pygame.event.Event(pygame.VIDEORESIZE,
                                  size=(event.width, event.height),
                                  width=event.width, height=event.height)
-        pygame.event.post(evt)
+            pygame.event.post(evt)
         return False  # continue processing
 
     def _quit_cb(self, data=None):
-        self.__stopped = True
         pygame.event.post(pygame.event.Event(pygame.QUIT))
 
     def _keydown_cb(self, widget, event):
@@ -148,7 +166,7 @@ class Translator(object):
 
     def _keymods(self):
         mod = 0
-        for key_val, mod_val in self.mod_map.iteritems():
+        for key_val, mod_val in self.mod_map.items():
             mod |= self.__keystate[key_val] and mod_val
         return mod
 
@@ -167,9 +185,9 @@ class Translator(object):
             keycode = getattr(pygame, 'K_' + key.lower())
         elif key == 'XF86Start':
             # view source request, specially handled...
-            self._mainwindow.view_source()
+            self._activity.view_source()
         else:
-            print 'Key %s unrecognized' % key
+            logging.error('Key %s unrecognized' % key)
 
         if keycode is not None:
             if type == pygame.KEYDOWN:
@@ -177,10 +195,10 @@ class Translator(object):
             self.__keystate[keycode] = type == pygame.KEYDOWN
             if type == pygame.KEYUP:
                 mod = self._keymods()
-            ukey = unichr(Gdk.keyval_to_unicode(event.keyval))
+            ukey = chr(Gdk.keyval_to_unicode(event.keyval))
             if ukey == '\000':
                 ukey = ''
-            evt = pygame.event.Event(type, key=keycode, unicode=ukey, mod=mod)
+            evt = pygame.event.Event(type, key=keycode, str=ukey, mod=mod)
             self._post(evt)
 
         return True
@@ -260,9 +278,11 @@ class Translator(object):
     def _post(self, evt):
         try:
             pygame.event.post(evt)
-        except pygame.error, e:
-            if str(e) == 'Event queue full':
-                print "Event queue full!"
+        except pygame.error as e:
+            if str(e) == 'video system not initialized':
+                pass
+            elif str(e) == 'Event queue full':
+                logging.error("Event queue full!")
                 pass
             else:
                 raise e
